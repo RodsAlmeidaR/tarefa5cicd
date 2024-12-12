8dbac5c84996d4e7263e638030446b576b415e23
RodsAlmeidaR <rodsalmeidar@gmail.com>
2024-12-11 21:45:46 -0300

    add

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -5,7 +5,7 @@ from calculadora import calcular_imc
 class TestCalculadora(unittest.TestCase):
 
     def test_soma(self):
-        self.assertEqual(calcular_imc(65, 1.74), 21.47)
+        self.assertEqual(calcular_imc(10, 1), 10)
 
 if __name__ == "__main__":
     unittest.main()
